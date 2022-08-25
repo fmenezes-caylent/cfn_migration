563d708cde8c58abec0df6cbecfb12ea14c42c95
Felipe Menezes <felipe.menezes@caylent.com>
2022-08-25 16:12:03 -0300

    Imports existing stack to cdk

--- test_cfn_migration/test_cfn_migration_stack.py
+++ test_cfn_migration/test_cfn_migration_stack.py
@@ -1,19 +1,16 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+
 )
 from constructs import Construct
+from aws_cdk.cloudformation_include import CfnInclude
+import os
 
 class TestCfnMigrationStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "TestCfnMigrationQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        cfn_template = CfnInclude(self, "Template",
+            template_file=os.path.join(os.getcwd(), "test_cfn_migration/templates/existing_stack.yaml")
+        )
